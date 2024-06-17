4b6532c43577a40b3e364ace1bbfb79ec4dd4e0e
Carddera1842 <carddera1842@gmail.com>
2024-06-17 11:00:01 -0500

    stringing characters

--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -7,5 +7,6 @@ console.log(word.toUpperCase());
 
 //What does ``word.slice(4).toUpperCase()`` return?
 
+console.log(word.slice(4).toUpperCase());
 
 //Experiment with other combinations (chains) of string methods.
