9a9812c3fab39d0e05aaeca6af0c36fdafec769e
Carddera1842 <carddera1842@gmail.com>
2024-08-05 13:16:03 -0500

    "DOM Exercises"

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+     missionAbort.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "Red";
+     });
+
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener("click", event => {
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+     });
+
+
+
 }
 
 window.addEventListener("load", init);
